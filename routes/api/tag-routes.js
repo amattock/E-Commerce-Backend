a8062ce1a05d999dd3e1fa5ab9ac858a9fe2ce78
amattock <134568545+amattock@users.noreply.github.com>
2023-09-24 21:14:26 -0500

    still working on POST

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -39,15 +39,26 @@ router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
   Tag.update({
     tag_name: req.body.tag_name,
+  },
+  {
+    where: {
+      id: req.params.id
+    },
+    })
+    .then((dbTagData) => {
+      res.json(dbTagData);
   });
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
-  tag.destroy({
+  Tag.destroy({
     where: {
       id: req.params.id,
     },
+  })
+  .then((dbTagData) => {
+    res.json(dbTagData);
   });
 });
 
